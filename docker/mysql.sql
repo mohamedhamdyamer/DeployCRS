e5f1bb45372c6b3474f62b49c047d969ec9cfa6f
root <root@DeployCRS.mshome.net>
2022-05-21 15:12:08 +0200

    re-organizing files

---
+++ docker/mysql.sql
@@ -0,0 +1,9 @@
+use mysql;
+
+Create Table Items (ProjectID varchar(3), Sequence int, Item varchar(30));
+
+Insert into Items (ProjectID, Sequence, Item) Values ("001", 1, "VPC");
+Insert into Items (ProjectID, Sequence, Item) Values ("001", 2, "DataDomain");
+Insert into Items (ProjectID, Sequence, Item) Values ("001", 3, "CRM");
+Insert into Items (ProjectID, Sequence, Item) Values ("001", 4, "CyberSense");
+Insert into Items (ProjectID, Sequence, Item) Values ("001", 5, "Test");
